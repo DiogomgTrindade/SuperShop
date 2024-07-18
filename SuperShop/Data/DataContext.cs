5abacadcadab531d37991b1e172c27781b417013
DiogomgTrindade <diogovsky1904@gmail.com>
2024-07-18 15:46:59 +0100

    First controller

---
+++ SuperShop/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop.Data.Entities;
+
+namespace SuperShop.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+    }
+}
