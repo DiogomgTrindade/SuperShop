d4d4dbc109f3e359ce0a207eb1f12da0eec2940f
DiogomgTrindade <diogovsky1904@gmail.com>
2024-07-22 14:19:07 +0100

    Modify products

--- SuperShop/Controllers/ProductsController.cs
+++ SuperShop/Controllers/ProductsController.cs
@@ -114,6 +114,8 @@ namespace SuperShop.Controllers
             return View(product);
         }
 
+
+
         // GET: Products/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -131,6 +133,8 @@ namespace SuperShop.Controllers
 
             return View(product);
         }
+
+
 
         // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -9,6 +9,8 @@ namespace SuperShop.Data.Entities
     {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage ="The field {0} can contain {1} characters length.")]
         public string Name { get; set; }
 
 
@@ -21,11 +23,11 @@ namespace SuperShop.Data.Entities
 
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase {  get; set; }
+        public DateTime? LastPurchase {  get; set; }
 
 
         [Display(Name= "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
 
         [Display(Name ="Is Available")]
