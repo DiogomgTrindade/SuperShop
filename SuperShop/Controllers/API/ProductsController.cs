97897d1612a6667108a88522e75085fce5e73b64
DiogomgTrindade <diogovsky1904@gmail.com>
2024-07-25 15:04:38 +0100

    API products controller

---
+++ SuperShop/Controllers/API/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperShop.Data;
+
+namespace SuperShop.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_productRepository.GetAll());
+        }
+
+    }
+}
